688ab69bb2617cd1a640dce2909f9d15a577860d
arty gvozdenkov <gvozdenkov@gmail.com>
2024-07-24 10:56:28 +0300

    feat(backend): init nestjs & gateway.module

---
+++ backend/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT ?? 3000;
+
+  await app.listen(PORT, () =>
+    console.log(`\nServer running on port ${PORT}, http://localhost:${PORT}\n`),
+  );
+}
+
+bootstrap();
