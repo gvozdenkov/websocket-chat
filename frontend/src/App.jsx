219861588c7159e8d95150f18140faaa600a56db
arty gvozdenkov <gvozdenkov@gmail.com>
2024-07-24 16:51:54 +0300

    refactor(frontend): move websocket context provider to main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
 import { Websocket } from '../components/websocket';
-import { socket, WebSocketProvider } from '../contexts/websocket-context';
 
 function App() {
-  return (
-    <WebSocketProvider value={socket}>
-      <Websocket />
-    </WebSocketProvider>
-  );
+  return <Websocket />;
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { socket, WebSocketProvider } from '../contexts/websocket-context.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WebSocketProvider value={socket}>
+      <App />
+    </WebSocketProvider>
   </React.StrictMode>
 );
